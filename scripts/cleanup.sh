83320b1a4c4eb61fdfc482c461f7f63f439d8c36
Harsh Desai <harsh@portworx.com>
2017-05-02 15:00:33 -0700

    fix cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-docker stop px-dev; docker rm px-dev
+docker stop $(docker ps -a -q --filter ancestor=portworx/px-enterprise:latest)
+docker rm $(docker ps -a -q --filter ancestor=portworx/px-enterprise:latest)
+
+docker stop $(docker ps -a -q --filter ancestor=portworx/px-dev:latest)
+docker rm $(docker ps -a -q  --filter ancestor=portworx/px-dev:latest)
+
+docker stop $(docker ps -a -q --filter ancestor=harshpx/px:latest)
+docker rm $(docker ps -a -q  --filter ancestor=harshpx/px:latest)
+
 docker stop etcd; docker rm etcd
 sudo rm -rf /etc/pwx/.private.json /etc/pwx/config.json
 
