bab78e3aa8370fa5a36b755df963cad74ea1d111
Harsh Desai <harsh@portworx.com>
2017-05-17 13:02:19 -0700

    add px service account px daemonset

--- scripts/namespaces/demo-namespace.sh
+++ scripts/namespaces/demo-namespace.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+echo "Creating namespaces and contexts..."
 kubectl create -f dev-namespace.yaml
 kubectl create -f prod-namespace.yaml
 kubectl config set-context dev --namespace=dev --cluster=kubernetes --user=kubernetes-admin
 kubectl config set-context prod --namespace=prod --cluster=kubernetes --user=kubernetes-admin
 
+echo "Switching to prod context..."
+kubectl config use-context prod
+
+kubectl create --namespace prod -f ../storage-class/px-sc-all.yaml
+
+sleep 5
+
+kubectl describe storageclass portworx-io-priority-high
+kubectl describe pvc
+kubectl describe pv
+kubectl get pod pvpod
+
+echo "Switching to dev context..."
+kubectl config use-context dev
+kubectl describe pvc
+kubectl describe pv
--- scripts/storage-class/demo-storage-class-with-roles.sh
+++ scripts/storage-class/demo-storage-class-with-roles.sh
@@ -2,6 +2,8 @@
 
 kubectl create -f px-sc-all.yaml
 
+sleep 5
+
 kubectl describe storageclass portworx-io-priority-high
 kubectl describe pvc
 kubectl describe pv
