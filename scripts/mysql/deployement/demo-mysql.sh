506a82a475883e3c0c629b0a4e71aebf5f40939d
Harsh Desai <harsh@portworx.com>
2017-07-28 13:53:18 -0700

    split mysql spec

--- scripts/mysql/deployement/demo-mysql.sh
+++ scripts/mysql/deployement/demo-mysql.sh
@@ -3,8 +3,11 @@
 echo mysql123 > password.txt
 tr --delete '\n' <password.txt >.strippedpassword.txt && mv .strippedpassword.txt password.txt
 kubectl create secret generic mysql-pass --from-file=password.txt
-kubectl apply -f px-mysql.yaml
+
+kubectl apply -f px-mysql-vol.yaml
+kubectl apply -f px-mysql-app.yaml
 
 sleep 5
 
-kubectl describe -f px-mysql.yaml
+kubectl describe -f px-mysql-vol.yaml
+kubectl describe -f px-mysql-app.yaml
--- scripts/mysql/deployement/undemo-mysql.sh
+++ scripts/mysql/deployement/undemo-mysql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 
-kubectl delete -f px-mysql.yaml
+kubectl delete -f px-mysql-app.yaml
+kubectl delete -f px-mysql-vol.yaml
 kubectl delete secret mysql-pass
