c6b4d98b27ef9bc7f37fa63ea693d7b31e456273
Harsh Desai <harsh@portworx.com>
2017-08-27 16:37:38 -0700

    refactor wp demo

--- scripts/wordpress/demo-px-wordpress.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ex
-
-tr --delete '\n' <password.txt >.strippedpassword.txt && mv .strippedpassword.txt password.txt
-kubectl create secret generic mysql-pass --from-file=password.txt
-kubectl apply -f px-mysql.yaml
-kubectl apply -f px-qp-app.yaml
