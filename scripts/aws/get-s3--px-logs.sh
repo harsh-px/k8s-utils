beaeaf30c28d698f1f5d2bad76f299f2e221b016
Harsh Desai <harsh@portworx.com>
2017-09-28 12:23:56 -0700

    add basic script to pull px s3 logs

---
+++ scripts/aws/get-s3--px-logs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DEFAULT_DUMP_LOCATION=/tmp/px-s3-logs-`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`
+
+function usage() {
+    echo "USAGE: ${0} -b bucket [-p <bucket-prefix>]"
+    exit 1
+}
+
+while getopts p:b: option
+do
+    case "${option}"
+    in
+    p) PREFIX=${OPTARG};;
+    b) BUCKET=${OPTARG};;
+    esac
+done
+
+if [ -z "$BUCKET" ]
+then
+    echo "Error: S3 bucket not given"
+    usage
+fi
+
+if [ -z "$" ]
+then
+    echo "Defaulting to empty bucket prefix"
+fi
+
+mkdir -p ${DEFAULT_DUMP_LOCATION}
+
+echo "Fetching S3 objects from bucket:${BUCKET} with prefix:${PREFIX}"
+
+for obj in `aws s3api list-objects --bucket "${BUCKET}" --prefix "${PREFIX}"  --query "sort_by(Contents,&LastModified)" --output json | jq -c  '.[] | .Key'`; do
+    aws s3api get-object --bucket ${BUCKET} --key "${obj//\"}" ${DEFAULT_DUMP_LOCATION}/$(basename "${obj//\"}")
+
+    #Extract
+    gzip -cd ${DEFAULT_DUMP_LOCATION}/$(basename "${obj//\"}") >> ${DEFAULT_DUMP_LOCATION}/master.json
+done
+
+# TODO Post to ES
+# cat ${DEFAULT_DUMP_LOCATION}/master.json | jq -c '. | {"index": {"_index": "logs", "_type": "log",}}, .' | curl -XPOST localhost:9200/_bulk --data-binary @-
