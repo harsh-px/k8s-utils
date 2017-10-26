dff24fbf069e118ca39621356001276b84924d05
Harsh Desai <harsh@portworx.com>
2017-10-25 17:15:42 -0700

    Add fluentd init container

---
+++ scripts/efk/fluentd/fluentd-alpine-utils/init-fluentd.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+PX_SERVICE="portworx-service.kube-system.svc.cluster.local"
+if [ -n "$1" ]; then
+    PX_SERVICE="$1"
+fi
+
+echo "Using service: $PX_SERVICE"
+
+pxClusterStatus=$(curl -XGET http://$PX_SERVICE:9001/v1/cluster/enumerate)
+
+while [[ "$pxClusterStatus" = "" ]]
+do
+ echo "Waiting... looks like the px pods arent ready yet"
+ sleep 1
+ pxClusterStatus=$(curl -XGET http://$PX_SERVICE:9001/v1/cluster/enumerate)
+done
+
+echo $pxClusterStatus
+echo $pxClusterStatus >> /usr/bin/clusterState
+
+indexID=$(echo $pxClusterStatus | jq '.["Id"]+"-"+.["UID"]')
+
+echo "Using index ID: $indexID"
+
+cat /tmp/fluent.conf
+
+sed -i "s/#indexUUID#/$indexID/g" /tmp/fluent.conf
