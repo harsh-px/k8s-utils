e3ae06b6165d62db48e1db401918cd0b2025d0cc
Harsh Desai <harsh@portworx.com>
2018-11-06 15:55:51 -0800

    fix command
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- scripts/monitoring/prometheus-all/monitoring.sh
+++ scripts/monitoring/prometheus-all/monitoring.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex 
 
-kubectl create secret generic alertmanager-portworx --from-file=alertmanager.yaml -n kube-system
+kubectl create secret generic alertmanager-portworx --from-file=alertmanager-cfg.yaml -n kube-system
 kubectl apply -f .
